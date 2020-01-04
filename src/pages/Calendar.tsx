8fc00be7ee2870b63a282bbf40dcbb88db75bcbf
erados <imdiots@gmail.com>
2020-01-04 16:59:04 +0900

    calendar updated

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -10,13 +10,27 @@ import './Calendar.css';
 class Calendar extends Component {
     state: any = {
         data:
-            [{id: 1, name: "1", checked: false}, {id: 2, name: "2", checked: false}, {id: 3, name: "3", checked: true}, {id: 4, name: "4", checked: true}, {id: 5, name: "5", checked: true}]
-    };
+            [{id: 1, name: "1", checked: false}, {id: 2, name: "2", checked: false}, {
+                id: 3,
+                name: "3",
+                checked: true
+            }, {id: 4, name: "4", checked: true}, {id: 5, name: "5", checked: true}]
+    }
 
-    date = new Date();
-    firstDay = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDate();
-    lastDay = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
-    offset = this.date.getDay();
+    createCalendar() {
+        let date = new Date();
+        let children = [];
+        let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+        let firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDate();
+        let offset = date.getDate();
+        for (let i = firstDay - offset + 1; i < lastDay; i+=7) {
+            let temp = [];
+            for(let j = i; j < i+7; j++)
+                temp.push(<IonCol>{(j > 0) ? ((j <= lastDay) ? j:"") : ""}</IonCol>);
+            children.push(<IonRow>{temp}</IonRow>);
+        }
+        return children;
+    }
 
     constructor(props: any) {
         super(props);
@@ -36,14 +50,7 @@ class Calendar extends Component {
         return (
             <IonContent>
                 <IonGrid>
-                    <IonRow>
-                        <IonCol>{this.firstDay}</IonCol>
-                        <IonCol>ion-col</IonCol>
-                        <IonCol>ion-col</IonCol>
-                        <IonCol>ion-col</IonCol>
-                        <IonCol>ion-col</IonCol>
-                        <IonCol>ion-col</IonCol>
-                    </IonRow>
+                    {this.createCalendar()}
                     <IonList>
                         <IonListHeader>List to be done</IonListHeader>
                         {this.state.data.map((obj: any, idx: number) => {
